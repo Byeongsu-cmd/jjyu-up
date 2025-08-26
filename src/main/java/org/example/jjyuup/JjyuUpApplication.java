e0a126e155976571a59398a8a1dc7fe769022b73
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-08-26 15:05:43 +0900

    first commit

---
+++ src/main/java/org/example/jjyuup/JjyuUpApplication.java
@@ -0,0 +1,13 @@
+package org.example.jjyuup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjyuUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JjyuUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jjyuup/JjyuUpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jjyuup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjyuUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
